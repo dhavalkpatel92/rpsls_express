40e0aa3b9266f75e3fc29de75ea433461b32d9a8
nikuldchauhan <dhaval301092@gmail.com>
2015-03-22 15:32:55 -0700

    rendor json and bootstrap alert are added

--- public/js/client.js
+++ public/js/client.js
@@ -4,19 +4,27 @@ $( "#rock_button,#paper_button,#scissors_button,#lizard_button,#spock_button" ).
   $.ajax({
    url: 'http://localhost:3000/play/'+id,
    type: 'post',
-   dataType: 'html',
+   dataType: 'json',
    contentType: "application/json; charset=utf-8",
    success: function(data) {
-     console.log("here want data");
-     console.log(data);
-      $("#result").html(data);
+     console.log(data.outcome);
+     if(data.outcome==="win")
+     {
+       $("#info").html('<div class="alert alert-success alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Won!</strong>You won for <strong>'+id+'</strong>.</div>');
+     }
+     else if(data.outcome==="losses")
+     {
+       $("#info").html('<div class="alert alert-danger alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Loss!</strong>You loss for <strong>'+id+'</strong>.</div>');
+     }
+     else if(data.outcome==="ties")
+     {
+       $("#info").html('<div class="alert alert-warning alert-dismissible"><a href="#" class="close" data-dismiss="alert">&times;</a><strong>Tie!</strong>You tie for <strong>'+id+'</strong>.</div>');
+     }
+     else
+     {
+       $("#info").html();
+     }
+     $("#result").html(renderjson.set_show_by_default(true)(data));
    }
  });
 });
-/*
-$.post("http://localhost:3000/play/"+id,
-       function(data){
-           alert("Data: " + data);
-       });
-});
-*/
